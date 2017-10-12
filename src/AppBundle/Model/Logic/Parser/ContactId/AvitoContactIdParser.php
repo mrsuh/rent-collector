237e88fbdfa4e243a0baa247280cb578d194d94f
anton <mrsuh6@gmail.com>
2017-10-12 10:30:41 +0300

    av contact id

--- src/AppBundle/Model/Logic/Parser/ContactId/AvitoContactIdParser.php
+++ src/AppBundle/Model/Logic/Parser/ContactId/AvitoContactIdParser.php
@@ -18,7 +18,24 @@ class AvitoContactIdParser implements ContactIdParserInterface
             throw new ParseException(sprintf('%s: data is not an instance of %s', __CLASS__ . '\\' . __FUNCTION__, Dom::class));
         }
 
-        return str_replace(uniqid('av', true), '.', '-');
+        $id = str_replace(uniqid('av', true), '.', '-');
+
+        $links = $data->find('.seller-info-name a');
+
+        if (!array_key_exists(0, $links)) {
+
+            return $id;
+        }
+
+        $link = $links[0];
+
+        preg_match('/\/user\/(.*)\/profile/', $link->href, $match);
+
+        if (array_key_exists(1, $match)) {
+            $id = 'av' . $match[1];
+        }
+
+        return $id;
     }
 }
 
